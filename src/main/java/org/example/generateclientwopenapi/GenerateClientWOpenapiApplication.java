4cc273f0004d054cf37769fbc23d41276408f19a
Benjamin Kostka <mail@ben-kostka.de>
2024-12-17 14:43:14 +0100

    Initial commit

---
+++ src/main/java/org/example/generateclientwopenapi/GenerateClientWOpenapiApplication.java
@@ -0,0 +1,13 @@
+package org.example.generateclientwopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateClientWOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateClientWOpenapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/generateclientwopenapi/GenerateClientWOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.generateclientwopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateClientWOpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
